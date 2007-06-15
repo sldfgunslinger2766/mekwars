a4c6aa45aa735f2a732bf42930715b17714979a6
torren <torren@c06f3bff-5232-0410-aa4b-b2ae7373f14e>
2007-06-15 17:14:06 +0000

    Release Server & Client 0.2.4.0

--- trunk/src/client/MWClient.java
+++ trunk/src/client/MWClient.java
@@ -148,7 +148,7 @@ public final class MWClient implements IClient {
 	
 	CConfig Config;
 	
-	public static final String CLIENT_VERSION = "0.2.3.0"; //change this with all client changes @Torren
+	public static final String CLIENT_VERSION = "0.2.4.0"; //change this with all client changes @Torren
 
 	CConnector Connector;
 	TimeOutThread TO;
