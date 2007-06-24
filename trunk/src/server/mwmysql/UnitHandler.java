5a3720044d5d91f1c4f5df9292ffa94e7e78eab5
billypinhead <billypinhead@c06f3bff-5232-0410-aa4b-b2ae7373f14e>
2007-06-24 00:47:24 +0000

    + Units and pilots saved to database

--- trunk/src/server/mwmysql/UnitHandler.java
+++ trunk/src/server/mwmysql/UnitHandler.java
@@ -1,15 +1,151 @@
 package server.mwmysql;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
+import common.util.UnitUtils;
+
+import megamek.common.AmmoType;
+import megamek.common.Entity;
+import megamek.common.Mech;
+import megamek.common.MiscType;
+import megamek.common.Mounted;
+import megamek.common.WeaponType;
+
+import server.MMServ;
+import server.campaign.CampaignMain;
 import server.campaign.SUnit;
+import server.campaign.pilot.SPilot;
 
 public class UnitHandler {
 	
 	Connection con;
 	
 	public void saveUnit(SUnit u) {
+	PreparedStatement ps;
+	ResultSet rs = null;
+	StringBuffer sql = new StringBuffer();
+	Entity ent = u.getEntity();
+	
+	try {
+		ps = con.prepareStatement("SELECT COUNT(*) as num from units where uid=?");
+		ps.setInt(1, u.getId());
+		rs = ps.executeQuery();
+		rs.next();
+		if(rs.getInt("num")==0) {
+			// Unit's not in there - insert it
+			sql.setLength(0);
+			sql.append("INSERT into units set uID=?, uFileName=?, uPosID=?, uStatus=?, uProducer=?, uWeightClass=?, uAutoEject=?, uHasSpotlight=?, uIsUsingSpotlight=?, uTargetSystem=?, uScrappableFor=?, uBattleDamage=?, uLastCombatPilot=?, uCurrentRepairCost=?, uLifetimeRepairCost=?");
+			ps=con.prepareStatement(sql.toString());
+			ps.setInt(1, u.getId());
+			ps.setString(2, u.getUnitFilename());
+			ps.setInt(3, u.getPosId());
+			ps.setInt(4, u.getStatus());
+			ps.setString(5, u.getProducer());
+			ps.setInt(6, u.getWeightclass());
+			if(ent instanceof Mech)
+			  ps.setString(7, Boolean.toString(((Mech)ent).isAutoEject()));
+			else
+				ps.setString(7, "false");
+			ps.setString(8, Boolean.toString(ent.hasSpotlight()));
+			ps.setString(9, Boolean.toString(ent.isUsingSpotlight()));
+			if (CampaignMain.cm.getData().getBannedTargetingSystems().containsKey(u.getEntity().getTargSysType())) {
+				ps.setInt(10, MiscType.T_TARGSYS_STANDARD);
+				u.getEntity().setTargSysType(MiscType.T_TARGSYS_STANDARD);
+			} else {
+				ps.setInt(10, ent.getTargSysType());
+			}
+			ps.setInt(11, u.getScrappableFor());
+			if(CampaignMain.cm.isUsingAdvanceRepair())
+				ps.setString(12, UnitUtils.unitBattleDamage(u.getEntity(), false));
+			else
+				ps.setString(12, "%%-%%-%%-");
+			ps.setInt(13, u.getLastCombatPilot());
+			ps.setInt(14, u.getCurrentRepairCost());
+			ps.setInt(15, u.getLifeTimeRepairCost());
+			ps.executeUpdate();
+		} else {
+			// Unit's already there - update it
+			sql.setLength(0);
+			sql.append("UPDATE units set uFileName=?, uPosID=?, uStatus=?, uProducer=?, uWeightClass=?, uAutoEject=?, uHasSpotlight=?, uIsUsingSpotlight=?, uTargetSystem=?, uScrappableFor=?, uBattleDamage=?, uLastCombatPilot=?, uCurrentRepairCost=?, uLifetimeRepairCost=? where uID=?");
+			ps=con.prepareStatement(sql.toString());
+			ps.setString(1, u.getUnitFilename());
+			ps.setInt(2, u.getPosId());
+			ps.setInt(3, u.getStatus());
+			ps.setString(4, u.getProducer());
+			ps.setInt(5, u.getWeightclass());
+			if(ent instanceof Mech)
+			  ps.setString(6, Boolean.toString(((Mech)ent).isAutoEject()));
+			else
+				ps.setString(6, "false");
+			ps.setString(7, Boolean.toString(ent.hasSpotlight()));
+			ps.setString(8, Boolean.toString(ent.isUsingSpotlight()));
+			if (CampaignMain.cm.getData().getBannedTargetingSystems().containsKey(u.getEntity().getTargSysType())) {
+				ps.setInt(9, MiscType.T_TARGSYS_STANDARD);
+				u.getEntity().setTargSysType(MiscType.T_TARGSYS_STANDARD);
+			} else {
+				ps.setInt(9, ent.getTargSysType());
+			}
+			ps.setInt(10, u.getScrappableFor());
+			if(CampaignMain.cm.isUsingAdvanceRepair())
+				ps.setString(11, UnitUtils.unitBattleDamage(u.getEntity(), false));
+			else
+				ps.setString(11, "%%-%%-%%-");
+			ps.setInt(12, u.getLastCombatPilot());
+			ps.setInt(13, u.getCurrentRepairCost());
+			ps.setInt(14, u.getLifeTimeRepairCost());
+			ps.setInt(15, u.getId());
+			ps.executeUpdate();
+		}
+		// Now do Machine Guns
+		ps.executeUpdate("DELETE from unit_mgs WHERE unitID = " + u.getId());
+		ArrayList<Mounted> en_Weapon = ent.getWeaponList();
+		int location = 0;
+		for (Mounted mWeapon : en_Weapon) {
+			WeaponType weapon = (WeaponType)mWeapon.getType();
+			if (weapon.hasFlag(WeaponType.F_MG)) {
+				sql.setLength(0);
+				sql.append("INSERT into unit_mgs set unitID=?, mgLocation=?, mgRapidFire=?");
+				ps = con.prepareStatement(sql.toString());
+				ps.setInt(1, u.getId());
+				ps.setInt(2, location);
+				ps.setString(3, Boolean.toString(mWeapon.isRapidfire()));
+				ps.executeUpdate();
+			}
+			location++;
+		}
+		// Do Ammo
+		ps.executeUpdate("DELETE from unit_ammo WHERE unitID = " + u.getId());
 		
+		ArrayList<Mounted> en_Ammo = ent.getAmmo();
+		for (Mounted mAmmo : en_Ammo ) {
+			boolean hotloaded = mAmmo.isHotLoaded();
+			if (!CampaignMain.cm.getMegaMekClient().game.getOptions().booleanOption("maxtech_hotload"))
+				hotloaded = false;
+			AmmoType at = (AmmoType)mAmmo.getType();
+			int AmmoLoc = mAmmo.getLocation();
+			sql.setLength(0);
+			sql.append("INSERT into unit_ammo set unitID = ?, ammoLocation = ?, ammoHotLoaded=?, ammoType=?, ammoInternalName=?, ammoShotsLeft=?");
+			ps = con.prepareStatement(sql.toString());
+			ps.setInt(1, u.getId());
+			ps.setInt(2, AmmoLoc);
+			ps.setString(3, Boolean.toString(hotloaded));
+			ps.setInt(4, at.getAmmoType());
+			ps.setString(5, at.getInternalName());
+			ps.setInt(6, mAmmo.getShotsLeft());
+			ps.executeUpdate();
+		}
+		// Save the pilot
+		if (u.getPilot().getGunnery()!=99){
+			CampaignMain.cm.MySQL.savePilot((SPilot)u.getPilot());
+			CampaignMain.cm.MySQL.linkPilotToUnit(u.getPilot().getPilotId(), u.getId());
+		}
+	} catch (SQLException e){
+		MMServ.mmlog.dbLog("SQL Exception in UnitHandler.saveUnit: " + e.getMessage());
+	}
 	}
 	
 	public UnitHandler(Connection c) {
