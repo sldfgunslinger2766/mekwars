8415e0209c086b26db134d9f4678611ca5845a17
billypinhead <billypinhead@c06f3bff-5232-0410-aa4b-b2ae7373f14e>
2007-08-15 15:35:55 +0000

    + Updated db_schema to v2

--- src/server/mwmysql/mysqlHandler.java
+++ src/server/mwmysql/mysqlHandler.java
@@ -25,7 +25,7 @@ public class mysqlHandler{
   private PlayerHandler plh = null;
   private PhpBBConnector phpBBCon = null;
 
-  private final int currentDBVersion = 1;
+  private final int currentDBVersion = 2;
   
   public void closeMySQL(){
 	  MySQLCon.close();
