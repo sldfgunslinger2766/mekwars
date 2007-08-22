08b5b962a02c2727039533148b030d2ac1da77d9
Torren <Torren@c06f3bff-5232-0410-aa4b-b2ae7373f14e>
2007-08-22 19:15:50 +0000

    + Added code to allow users to set unit commanders and see what units   where set as unit commanders + Added SetUnitCommandCommand

---
+++ src/server/campaign/commands/SetUnitCommanderCommand.java
@@ -0,0 +1,94 @@
+/*
+ * MekWars - Copyright (C) 2007 
+ * 
+ * Derived from MegaMekNET (http://www.sourceforge.net/projects/megameknet)
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ */
+
+/*
+ * Created on 08.22.2007
+ *  
+ */
+package server.campaign.commands;
+
+import java.util.StringTokenizer;
+
+import common.Unit;
+
+import server.campaign.CampaignMain;
+import server.campaign.SHouse;
+import server.campaign.SPlayer;
+import server.campaign.SUnit;
+
+/**
+ * @author Jason Tighe
+ *  
+ */
+public class SetUnitCommanderCommand implements Command {
+	
+	int accessLevel = 0;
+	public int getExecutionLevel(){return accessLevel;}
+	public void setExecutionLevel(int i) {accessLevel = i;}
+	
+	public void process(StringTokenizer command,String Username) {
+		
+		if (accessLevel != 0) {
+			int userLevel = CampaignMain.cm.getServer().getUserLevel(Username);
+			if(userLevel < getExecutionLevel()) {
+				CampaignMain.cm.toUser("Insufficient access level for command. Level: " + userLevel + ". Required: " + accessLevel + ".",Username,true);
+				return;
+			}
+		}
+		
+		SPlayer p = CampaignMain.cm.getPlayer(Username);
+		SHouse house = p.getMyHouse();
+		int mechid = -1;
+		
+		try {
+			mechid = Integer.parseInt((String)command.nextElement());
+		} catch (Exception e) {
+			CampaignMain.cm.toUser("Incorrect syntax. Try: /setUnitCommander unit ID", Username, true);
+			return;
+		}
+		
+		SUnit m = p.getUnit(mechid);
+		if (m == null) {
+			CampaignMain.cm.toUser("Could not find a unit with the given ID.", Username, true);
+			return;
+		}
+		
+		if (m.getStatus() == Unit.STATUS_FORSALE) {
+			CampaignMain.cm.toUser("Units that are for sale on the Market.", Username, true);
+			return;
+		}
+		
+		if ( m.getPilot() == null || m.getPilot().getName().equalsIgnoreCase("vacant") ){
+			CampaignMain.cm.toUser("This unit does not have a pilot to be a commander for!", Username);
+			return;
+		}
+		
+		if ( !house.getBooleanConfig("allowUnitCommander"+SUnit.getTypeClassDesc(m.getType())) ){
+			CampaignMain.cm.toUser(SUnit.getTypeClassDesc(m.getType())+" units are not allowed to be set as unit commanders!", Username);
+			return;
+		}
+		
+		if ( p.getAmountOfTimesUnitExistsInArmies(m.getId()) < 1){
+			CampaignMain.cm.toUser("the "+m.getModelName()+" is not in any armies!", Username);
+			return;
+		}
+		
+		m.setUnitCommander(true);
+		CampaignMain.cm.toUser("PL|UU|"+m.getId()+"|"+m.toString(true),Username,false);
+		CampaignMain.cm.toUser("Unit #"+m.getId()+" has been set as unit commander", Username);
+	}//end process()
+
+}//end ScrapCommand
