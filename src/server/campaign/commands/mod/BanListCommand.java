ae88bd9afde0a083c289114306ab3c888412d15d
Torren <Torren@c06f3bff-5232-0410-aa4b-b2ae7373f14e>
2007-08-23 15:00:53 +0000

    + Bug: Concurrent Modification error when using the BanListCommand.

--- src/server/campaign/commands/mod/BanListCommand.java
+++ src/server/campaign/commands/mod/BanListCommand.java
@@ -19,91 +19,96 @@ package server.campaign.commands.mod;
 import java.net.InetAddress;
 
 import java.util.Date;
-import java.util.Iterator;
 import java.util.StringTokenizer;
+import java.util.concurrent.ConcurrentHashMap;
 
 import server.campaign.CampaignMain;
 import server.campaign.commands.Command;
 import server.MWChatServer.auth.IAuthenticator;
 
-
 /**
  * Moving the BanList command from MMServ into the normal command structure.
- *
- * Syntax  /c BanList
+ * 
+ * Syntax /c BanList
  */
 public class BanListCommand implements Command {
-	
+
 	int accessLevel = IAuthenticator.MODERATOR;
-	public int getExecutionLevel(){return accessLevel;}
-	public void setExecutionLevel(int i) {accessLevel = i;}
-	
-	public void process(StringTokenizer command,String Username) {
-		
+
+	public int getExecutionLevel() {
+		return accessLevel;
+	}
+
+	public void setExecutionLevel(int i) {
+		accessLevel = i;
+	}
+
+	public void process(StringTokenizer command, String Username) {
+
 		if (accessLevel != 0) {
 			int userLevel = CampaignMain.cm.getServer().getUserLevel(Username);
-			if(userLevel < getExecutionLevel()) {
-				CampaignMain.cm.toUser("Insufficient access level for command. Level: " + userLevel + ". Required: " + accessLevel + ".",Username,true);
+			if (userLevel < getExecutionLevel()) {
+				CampaignMain.cm.toUser(
+						"Insufficient access level for command. Level: "
+								+ userLevel + ". Required: " + accessLevel
+								+ ".", Username, true);
 				return;
 			}
 		}
-		
 
-	        String result = "Banned accounts:<br>";
-	        int i = 1;
+		String result = "Banned accounts:<br>";
+		int i = 1;
 
-        synchronized (CampaignMain.cm.getServer().getBanAccounts()) {
-	        Iterator<String> banNames = CampaignMain.cm.getServer().getBanAccounts().keySet().iterator();
-	        Iterator<String> banTimes = CampaignMain.cm.getServer().getBanAccounts().values().iterator();
-	        while (banNames.hasNext()) {
-	            String banName = banNames.next();
-	            String banTime = banTimes.next();
-	            
-	            //Banned Accounts Username Check
-	            Long until = Long.valueOf(banTime);
-	            
-	            //If they are no longer banned remove them from the list and don't display them
-	            if (until.longValue() < System.currentTimeMillis() || until.longValue() == 0) {
-	                CampaignMain.cm.getServer().getBanAccounts().remove(banName);
-	                CampaignMain.cm.getServer().bansUpdate();
-	                continue;
-	            } 
-	            
-	            Long l = Long.valueOf(banTime);
-	            result += Integer.toString(i++);
-	            result +=  ") ";
-	            result +=  banName;
-	            result +=  " [unban at ";
-	            result +=  new Date(l).toString();
-	            result +=  "]<br>";
-	        }
+		ConcurrentHashMap<String, String> banHash = new ConcurrentHashMap<String, String>(
+				CampaignMain.cm.getServer().getBanAccounts());
+		for (String banName : banHash.keySet()) {
+			String banTime = banHash.get(banName);
+
+			// Banned Accounts Username Check
+			Long until = Long.valueOf(banTime);
+
+			// If they are no longer banned remove them from the list and don't
+			// display them
+			if (until.longValue() < System.currentTimeMillis()
+					|| until.longValue() == 0) {
+				CampaignMain.cm.getServer().getBanAccounts().remove(banName);
+				CampaignMain.cm.getServer().bansUpdate();
+				continue;
+			}
+
+			Long l = Long.valueOf(banTime);
+			result += Integer.toString(i++);
+			result += ") ";
+			result += banName;
+			result += " [unban at ";
+			result += new Date(l).toString();
+			result += "]<br>";
 		}
-		
+
 		i = 1;
-        result += "Banned IPs:<br>";
-		synchronized (CampaignMain.cm.getServer().getBanIps().keySet()) {
+		result += "Banned IPs:<br>";
+		ConcurrentHashMap<InetAddress, Long> banIpHash = new ConcurrentHashMap<InetAddress, Long>(
+				CampaignMain.cm.getServer().getBanIps());
 
-	        for (InetAddress currAddress : CampaignMain.cm.getServer().getBanIps().keySet()) {
-	            Long l = CampaignMain.cm.getServer().getBanIps().get(currAddress);
-	            
-	            //If they are no longer banned remove them from the list and don't display them
-	            if (l.longValue() < System.currentTimeMillis() || l.longValue() == 0) {
-	                CampaignMain.cm.getServer().getBanIps().remove(currAddress);
-	                CampaignMain.cm.getServer().bansUpdate();
-	                continue;
-	            } 
-	            
-	            result += i++
-	            + ") "
-	            + currAddress.toString()
-	            + " [unban at "
-	            + new Date(l.longValue()).toString()
-	            + "]<br>";
-	        }
+		for (InetAddress currAddress : banIpHash.keySet()) {
+			Long until = CampaignMain.cm.getServer().getBanIps().get(currAddress);
+
+			// If they are no longer banned remove them from the list and don't
+			// display them
+			if (until.longValue() < System.currentTimeMillis()
+					|| until.longValue() == 0) {
+				CampaignMain.cm.getServer().getBanIps().remove(currAddress);
+				CampaignMain.cm.getServer().bansUpdate();
+				continue;
+			}
+
+			result += i++ + ") " + currAddress.toString() + " [unban at "
+					+ new Date(until.longValue()).toString() + "]<br>";
 		}
-		
-        CampaignMain.cm.toUser(result, Username);
-        CampaignMain.cm.doSendModMail("NOTE",Username + " checked the ban list.");
-        //MMServ.mmlog.modLog(Username + " checked the ban list.");
+
+		CampaignMain.cm.toUser(result, Username);
+		CampaignMain.cm.doSendModMail("NOTE", Username
+				+ " checked the ban list.");
+		// MMServ.mmlog.modLog(Username + " checked the ban list.");
 	}
 }
