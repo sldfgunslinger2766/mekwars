77de4b5be50b2accf016e8c63ae13396805da88a
billypinhead <billypinhead@c06f3bff-5232-0410-aa4b-b2ae7373f14e>
2008-01-28 13:39:35 +0000

    Added a bunch of missing Copyright statements, mostly because I'm lazy about adding them when I create a class.  I'll try to be better about that.

--- src/server/campaign/commands/ValidateEmailCommand.java
+++ src/server/campaign/commands/ValidateEmailCommand.java
@@ -1,5 +1,5 @@
 /*
- * MekWars - Copyright (C) 2004 
+ * MekWars - Copyright (C) 2007 
  * 
  * Derived from MegaMekNET (http://www.sourceforge.net/projects/megameknet)
  * Original Author - Nathan Morris (urgru)
@@ -27,6 +27,23 @@ public class ValidateEmailCommand implements Command {
 	String syntax = "";
 	public int getExecutionLevel(){return accessLevel;}
 	public void setExecutionLevel(int i) {accessLevel = i;}
+	/*
+	 * MekWars - Copyright (C) 2004 
+	 * 
+	 * Original author - Bob Eldred (billypinhead@users.sourceforge.net)
+	 *
+	 * This program is free software; you can redistribute it and/or modify it
+	 * under the terms of the GNU General Public License as published by the Free
+	 * Software Foundation; either version 2 of the License, or (at your option)
+	 * any later version.
+	 *
+	 * This program is distributed in the hope that it will be useful, but
+	 * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+	 * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+	 * for more details.
+	 */
+
+	
 	public String getSyntax() { return syntax;}
 	
 	public void process(StringTokenizer command,String Username) {
--- src/server/mwmysql/MWmysql.java
+++ src/server/mwmysql/MWmysql.java
@@ -1,20 +1,21 @@
-/*
- * MekWars - Copyright (C) 2007  
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the Free
- * Software Foundation; either version 2 of the License, or (at your option)
- * any later version.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
- * for more details.
- */
+	/*
+	 * MekWars - Copyright (C) 2007 
+	 * 
+	 * Original author - Bob Eldred (billypinhead@users.sourceforge.net)
+	 *
+	 * This program is free software; you can redistribute it and/or modify it
+	 * under the terms of the GNU General Public License as published by the Free
+	 * Software Foundation; either version 2 of the License, or (at your option)
+	 * any later version.
+	 *
+	 * This program is distributed in the hope that it will be useful, but
+	 * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+	 * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+	 * for more details.
+	 */
 
-/**
- * @author Spork aka BillyPinHead
- */
+	
+
 package server.mwmysql;
 
 import java.io.IOException;
