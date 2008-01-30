e6358c719e698032d75077298a5e000a3a2a6528
billypinhead <billypinhead@c06f3bff-5232-0410-aa4b-b2ae7373f14e>
2008-01-30 02:25:36 +0000

    + Updated DB to v24

--- trunk/src/server/mwmysql/mysqlHandler.java
+++ trunk/src/server/mwmysql/mysqlHandler.java
@@ -42,7 +42,7 @@ public class mysqlHandler{
   private PlayerHandler plh = null;
   private PhpBBConnector phpBBCon = null;
 
-  private final int currentDBVersion = 23;
+  private final int currentDBVersion = 24;
   
   public void closeMySQL(){
 	  MySQLCon.close();
