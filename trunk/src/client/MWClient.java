8ede7e4ac85e69c150015f376c109bf3b551d2c3
Torren <Torren@c06f3bff-5232-0410-aa4b-b2ae7373f14e>
2008-03-04 19:06:55 +0000

    Release Client & Server 0.2.21.1 + New MegaMek.jar 0.33.21-MW0300408-5543

--- trunk/src/client/MWClient.java
+++ trunk/src/client/MWClient.java
@@ -159,7 +159,7 @@ public final class MWClient implements IClient {
 
     CConfig Config;
 
-    public static final String CLIENT_VERSION = "0.2.21.0"; // change this with
+    public static final String CLIENT_VERSION = "0.2.21.1"; // change this with
                                                             // all client
                                                             // changes @Torren
 
