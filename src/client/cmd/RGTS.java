a4a78770c0f9f12dabeb9df9538e50c1872ec43f
Torren <Torren@c06f3bff-5232-0410-aa4b-b2ae7373f14e>
2008-04-08 17:24:05 +0000

    Release Client 0.2.27.1 + removed Build Table viewer spam from Misc for Non-Staff + Bug: Build Table viewer not working for Players + Bug: Cored Meks not being left for salvage.

--- src/client/cmd/RGTS.java
+++ src/client/cmd/RGTS.java
@@ -27,7 +27,6 @@ import client.util.SerializeEntity;
 
 import megamek.common.Building;
 import megamek.common.Entity;
-import megamek.common.Mech;
 import megamek.common.Player;
 import megamek.server.Server;
 
@@ -35,129 +34,121 @@ import megamek.server.Server;
  * @author Imi (immanuel.scholz@gmx.de)
  */
 public class RGTS extends Command {
-	
-	private Server server = null;
-	/**
-	 * @param client
-	 */
-	public RGTS(MWClient mwclient) {
-		super(mwclient);
-	}
-	
-	/**
-	 * @see server.cmd.Command#execute(java.lang.String)
-	 */
-	@Override
-	public void execute(String input) {
-		
-		//CampaignData.mwlog.errLog("Inside RGTS");
-		server = mwclient.getMyServer();
-		StringBuilder result = new StringBuilder();
-		String name = "";
-		//Parse the real playername from the Modified In game one..
-		String winnerName = "";
-		if ( server.getGame().getVictoryTeam() != Player.TEAM_NONE) {
-			
-			int numberOfWinners = 0;
-			//Multiple Winners
-			Enumeration<Player> en = server.getGame().getPlayers();
-			while (en.hasMoreElements()) {
-				Player p = en.nextElement();
-				if (p.getTeam() == server.getGame().getVictoryTeam()) {
-					StringTokenizer st = new StringTokenizer(p.getName().trim(), "~");
-					name = "";
-					while (st.hasMoreElements()) {
-						name = st.nextToken().trim();
-					}
-                    //some of the players set themselves as a team of 1. 
-                    //This keeps that from happening.
-                    if ( numberOfWinners > 0 )
+
+    private Server server = null;
+
+    /**
+     * @param client
+     */
+    public RGTS(MWClient mwclient) {
+        super(mwclient);
+    }
+
+    /**
+     * @see server.cmd.Command#execute(java.lang.String)
+     */
+    @Override
+    public void execute(String input) {
+
+        // CampaignData.mwlog.errLog("Inside RGTS");
+        server = mwclient.getMyServer();
+        StringBuilder result = new StringBuilder();
+        String name = "";
+        // Parse the real playername from the Modified In game one..
+        String winnerName = "";
+        if (server.getGame().getVictoryTeam() != Player.TEAM_NONE) {
+
+            int numberOfWinners = 0;
+            // Multiple Winners
+            Enumeration<Player> en = server.getGame().getPlayers();
+            while (en.hasMoreElements()) {
+                Player p = en.nextElement();
+                if (p.getTeam() == server.getGame().getVictoryTeam()) {
+                    StringTokenizer st = new StringTokenizer(p.getName().trim(), "~");
+                    name = "";
+                    while (st.hasMoreElements()) {
+                        name = st.nextToken().trim();
+                    }
+                    // some of the players set themselves as a team of 1.
+                    // This keeps that from happening.
+                    if (numberOfWinners > 0)
                         winnerName += "*";
-					numberOfWinners++;
-					
-					winnerName += name;
-				}
-			}
-			if ( winnerName.endsWith("*") )
-			    winnerName = winnerName.substring(0,winnerName.length()-1);
-			winnerName += "#";
-		} 
-		
-		//Only one winner
-		else {
-			if (server.getGame().getVictoryPlayerId() == Player.PLAYER_NONE) {
-				winnerName = "DRAW#";
-			} else {
-				winnerName = server.getGame().getPlayer(server.getGame().getVictoryPlayerId()).getName();
-				StringTokenizer st = new StringTokenizer(winnerName, "~");
-				name = "";
-				while (st.hasMoreElements())
-					name = st.nextToken().trim();
-				winnerName = name + "#";
-			}
-		}
-		
-		result.append(winnerName);
-		
-		//Report the mech stat
-		Enumeration<Entity> en = server.getGame().getDevastatedEntities();
-		while (en.hasMoreElements()) {
-			Entity ent = en.nextElement();
-			if ( ent.getOwner().getName().startsWith("War Bot"))
-				continue;
-			result.append(SerializeEntity.serializeEntity(ent, true, false,mwclient.isUsingAdvanceRepairs()) + "#");
-		}
-		en = server.getGame().getGraveyardEntities();
-		while (en.hasMoreElements()) {
-			Entity ent = en.nextElement();
-			if ( ent.getOwner().getName().startsWith("War Bot"))
-				continue;
-			if (ent instanceof Mech && ent.getInternal(Mech.LOC_CT) <= 0)
-				result.append(SerializeEntity.serializeEntity(ent, true, true,mwclient.isUsingAdvanceRepairs()) + "#");              
-			else
-				result.append(SerializeEntity.serializeEntity(ent, true, false,mwclient.isUsingAdvanceRepairs()) + "#");
-			
-		}
-		en = server.getGame().getEntities();
-		while (en.hasMoreElements()) {
-			Entity ent = en.nextElement();
-			if ( ent.getOwner().getName().startsWith("War Bot"))
-				continue;
-			if (ent instanceof Mech && ent.getInternal(Mech.LOC_CT) <= 0)
-				result.append(SerializeEntity.serializeEntity(ent, true, false,mwclient.isUsingAdvanceRepairs()) + "#");          
-			else
-				result.append(SerializeEntity.serializeEntity(ent, true, false,mwclient.isUsingAdvanceRepairs()) + "#");
-		}
-		en = server.getGame().getRetreatedEntities();
-		while (en.hasMoreElements()) {
-			Entity ent = en.nextElement();
-			if ( ent.getOwner().getName().startsWith("War Bot"))
-				continue;
-			if (ent instanceof Mech && ent.getInternal(Mech.LOC_CT) <= 0)
-				result.append(SerializeEntity.serializeEntity(ent, true, false,mwclient.isUsingAdvanceRepairs()) + "#");          
-			else
-				result.append(SerializeEntity.serializeEntity(ent, true, false,mwclient.isUsingAdvanceRepairs()) + "#");
-		}
-		
-		if ( mwclient.getBuildingTemplate()!= null )
-			result.append("BL*"+this.getBuildingsLeft());
-		CampaignData.mwlog.infoLog("CR|" + result);
-		
-		//send the autoreport
-		mwclient.serverSend("CR|" + result.toString());
-		
-		//we may assume that a server which reports a game is no longer "Running"
-		mwclient.serverSend("SHS|" + mwclient.myUsername + "|Open");
-	}
-	
-	public int getBuildingsLeft(){
-		Enumeration<Building> buildings = server.getGame().getBoard().getBuildings();
-		int buildingCount = 0;
-		while ( buildings.hasMoreElements() ){
-			buildings.nextElement();
-			buildingCount++;
-		}
-		return buildingCount;
-	}
-	
+                    numberOfWinners++;
+
+                    winnerName += name;
+                }
+            }
+            if (winnerName.endsWith("*"))
+                winnerName = winnerName.substring(0, winnerName.length() - 1);
+            winnerName += "#";
+        }
+
+        // Only one winner
+        else {
+            if (server.getGame().getVictoryPlayerId() == Player.PLAYER_NONE) {
+                winnerName = "DRAW#";
+            } else {
+                winnerName = server.getGame().getPlayer(server.getGame().getVictoryPlayerId()).getName();
+                StringTokenizer st = new StringTokenizer(winnerName, "~");
+                name = "";
+                while (st.hasMoreElements())
+                    name = st.nextToken().trim();
+                winnerName = name + "#";
+            }
+        }
+
+        result.append(winnerName);
+
+        // Report the mech stat
+        Enumeration<Entity> en = server.getGame().getDevastatedEntities();
+        while (en.hasMoreElements()) {
+            Entity ent = en.nextElement();
+            if (ent.getOwner().getName().startsWith("War Bot"))
+                continue;
+            result.append(SerializeEntity.serializeEntity(ent, true, false, mwclient.isUsingAdvanceRepairs()) + "#");
+        }
+        en = server.getGame().getGraveyardEntities();
+        while (en.hasMoreElements()) {
+            Entity ent = en.nextElement();
+            if (ent.getOwner().getName().startsWith("War Bot"))
+                continue;
+            result.append(SerializeEntity.serializeEntity(ent, true, false, mwclient.isUsingAdvanceRepairs()) + "#");
+
+        }
+        en = server.getGame().getEntities();
+        while (en.hasMoreElements()) {
+            Entity ent = en.nextElement();
+            if (ent.getOwner().getName().startsWith("War Bot"))
+                continue;
+            result.append(SerializeEntity.serializeEntity(ent, true, false, mwclient.isUsingAdvanceRepairs()) + "#");
+        }
+        en = server.getGame().getRetreatedEntities();
+        while (en.hasMoreElements()) {
+            Entity ent = en.nextElement();
+            if (ent.getOwner().getName().startsWith("War Bot"))
+                continue;
+            result.append(SerializeEntity.serializeEntity(ent, true, false, mwclient.isUsingAdvanceRepairs()) + "#");
+        }
+
+        if (mwclient.getBuildingTemplate() != null)
+            result.append("BL*" + this.getBuildingsLeft());
+        CampaignData.mwlog.infoLog("CR|" + result);
+
+        // send the autoreport
+        mwclient.serverSend("CR|" + result.toString());
+
+        // we may assume that a server which reports a game is no longer "Running"
+        mwclient.serverSend("SHS|" + mwclient.myUsername + "|Open");
+    }
+
+    public int getBuildingsLeft() {
+        Enumeration<Building> buildings = server.getGame().getBoard().getBuildings();
+        int buildingCount = 0;
+        while (buildings.hasMoreElements()) {
+            buildings.nextElement();
+            buildingCount++;
+        }
+        return buildingCount;
+    }
+
 }
