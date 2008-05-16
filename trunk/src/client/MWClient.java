ec972c092d5a1b66b92432367a78f253d9ed7f75
Torren <Torren@c06f3bff-5232-0410-aa4b-b2ae7373f14e>
2008-05-16 17:56:00 +0000

    Release Client&Server 0.2.30.0 + new MekWarsDed.jar 0.2.30.0 + new MegaMek.jar 0.33.25-MW05162008-5673

--- trunk/src/client/MWClient.java
+++ trunk/src/client/MWClient.java
@@ -157,7 +157,7 @@ public final class MWClient implements IClient {
 
     CConfig Config;
 
-    public static final String CLIENT_VERSION = "0.2.29.0"; // change this with
+    public static final String CLIENT_VERSION = "0.2.30.0"; // change this with
                                                             // all client
                                                             // changes @Torren
 
