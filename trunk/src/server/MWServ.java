3c806e68c9ccd643227e8b5cecc5b96f6751ea68
billypinhead <billypinhead@c06f3bff-5232-0410-aa4b-b2ae7373f14e>
2011-01-06 19:02:45 +0000

    Changed bay loss due to unpaid techs/bays to only cut those you can't pay for

--- trunk/src/server/MWServ.java
+++ trunk/src/server/MWServ.java
@@ -54,7 +54,7 @@ import common.comm.ServerCommand;
 public class MWServ {
 
 	// Static logging engine, and static version info.
-    public static final String SERVER_VERSION = "0.3.2.5";// Sync this with the
+    public static final String SERVER_VERSION = "0.3.2.6";// Sync this with the
 															// clientVersion in
 															// MMClient @Torren
 
