0164b6796204d6c9188e155dd71d629324bdaae1
billypinhead <billypinhead@c06f3bff-5232-0410-aa4b-b2ae7373f14e>
2011-01-07 15:59:24 +0000

    Released Server v0.3.2.7, Client v0.3.2.4 RFE2028023: Unit Ratios based on Tech Level

--- src/client/MWClient.java
+++ src/client/MWClient.java
@@ -179,7 +179,7 @@ public final class MWClient implements IClient, GameListener {
 
     CConfig Config;
 
-    public static final String CLIENT_VERSION = "0.3.2.3"; // change this with
+    public static final String CLIENT_VERSION = "0.3.2.4"; // change this with
     // all client
     // changes @Torren
 
