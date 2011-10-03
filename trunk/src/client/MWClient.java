cad3f792160a63425e9b4e40920c07029b23a62f
billypinhead <billypinhead@c06f3bff-5232-0410-aa4b-b2ae7373f14e>
2011-10-03 18:55:47 +0000

    Released Server v0.3.5.5, Client v0.3.5.2 Bug: Units with quotes in the model did not work in MEKINFO links

--- trunk/src/client/MWClient.java
+++ trunk/src/client/MWClient.java
@@ -180,7 +180,7 @@ public final class MWClient implements IClient, GameListener {
 
     CConfig Config;
 
-    public static final String CLIENT_VERSION = "0.3.5.1"; // change this with
+    public static final String CLIENT_VERSION = "0.3.5.2"; // change this with
     // all client
     // changes @Torren
 
