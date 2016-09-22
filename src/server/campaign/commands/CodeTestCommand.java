92e97bbeb827c398287e772e2eec309dcb6b4c31
billypinhead <billypinhead@c06f3bff-5232-0410-aa4b-b2ae7373f14e>
2016-09-22 18:21:47 +0000

    Integrated Quartz Scheduler, started setting up a scheduling framework

--- src/server/campaign/commands/CodeTestCommand.java
+++ src/server/campaign/commands/CodeTestCommand.java
@@ -16,22 +16,47 @@
 
 package server.campaign.commands;
 
+import java.util.Date;
 import java.util.StringTokenizer;
 
-import server.campaign.CampaignMain;
+import org.quartz.JobDetail;
+import org.quartz.JobKey;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.Trigger;
+import org.quartz.TriggerKey;
 
-import common.campaign.operations.Operation;
+import static org.quartz.JobBuilder.*;
+import static org.quartz.TriggerBuilder.*;
+import static org.quartz.SimpleScheduleBuilder.*;
+import static org.quartz.CronScheduleBuilder.*;
+import static org.quartz.CalendarIntervalScheduleBuilder.*;
+import static org.quartz.JobKey.*;
+import static org.quartz.TriggerKey.*;
+import static org.quartz.DateBuilder.*;
+import static org.quartz.impl.matchers.KeyMatcher.*;
+import static org.quartz.impl.matchers.GroupMatcher.*;
+import static org.quartz.impl.matchers.AndMatcher.*;
+import static org.quartz.impl.matchers.OrMatcher.*;
+import static org.quartz.impl.matchers.EverythingMatcher.*;
+import common.CampaignData;
+
+
+import server.campaign.CampaignMain;
+import server.campaign.util.scheduler.RepeatingJob;
 
 public class CodeTestCommand implements Command {
 	
 	int accessLevel = 0;
 	public int getExecutionLevel(){return 200;}
 	public void setExecutionLevel(int i) {}
-	String syntax = "";
+	String syntax = "start or stop";
 	public String getSyntax() { return syntax;}
+
+	final int ACTION_START = 0;
+    final int ACTION_STOP = 1;
 	
 	public void process(StringTokenizer command,String Username) {
-		
 		if (accessLevel != 0) {
 			int userLevel = CampaignMain.cm.getServer().getUserLevel(Username);
 			if(userLevel < getExecutionLevel()) {
@@ -40,11 +65,60 @@ public class CodeTestCommand implements Command {
 			}
 		}
 		
-		for (Operation o : CampaignMain.cm.getOpsManager().getOperations().values()) {
-			String folder = "./data/operations/xml";
-			String fileName = o.getName() + ".xml";
-			o.writeToXmlFile(folder, fileName);
-			CampaignMain.cm.toUser("OP|add|" + o.getName() + "|" + o.getXmlString(), Username, false);
+		
+		int action;
+		
+		if(!command.hasMoreTokens()) {
+		  CampaignMain.cm.toUser("AM: invalid syntax: try /codetest start or /codetest stop", Username, false);
+		  return;
+		}
+		
+		String s = command.nextToken();
+		
+		if (s.equalsIgnoreCase("start")) {
+		  action = ACTION_START;	
+		} else if (s.equalsIgnoreCase("stop")) {
+		  action = ACTION_STOP;	
+		} else {
+			CampaignMain.cm.toUser("AM: invalid syntax: try /codetest start or /codetest stop", Username, false);
+			return;	
+		}
+		
+		switch (action) {
+		case ACTION_START:
+						
+			JobDetail job = newJob(RepeatingJob.class)
+							  .withIdentity("job1", "group1")
+							  .build();
+			Date runTime = evenMinuteDate(new Date());
+			
+			Trigger trigger = newTrigger()
+								.withIdentity("trigger1", "group1")
+								.withSchedule(cronSchedule("0/5 * * * * ?"))
+								.startAt(runTime)
+								.build();
+			
+			try {
+				CampaignMain.cm.getScheduler().scheduleJob(job, trigger);
+			} catch (SchedulerException e) {
+				CampaignData.mwlog.errLog(e);
+			}
+			
+			break;
+		case ACTION_STOP:
+			try {
+				CampaignMain.cm.getScheduler().unscheduleJob(new TriggerKey("trigger1", "group1"));
+				Scheduler sc = CampaignMain.cm.getScheduler();
+				for (String group : sc.getJobGroupNames()) {
+					for(JobKey jobKey : sc.getJobKeys(jobGroupEquals(group))) {
+						CampaignMain.cm.doSendModMail("SERVER", "Found Job Identified by: " + jobKey);
+					}
+				}
+				
+			} catch (SchedulerException e) {
+				CampaignData.mwlog.errLog(e);
+			}
+			break;
 		}
 	}
 }
